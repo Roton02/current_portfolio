6932f31d7221d2666b705c972b0dbc9cdb993eb3
Roton02 <sanaullahroton01@gmail.com>
2025-12-10 16:09:42 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   build: {
-    outDir: 'dist'
+    outDir: "dist",
   },
   optimizeDeps: {
-    include: ['react-parallax-tilt'], // বা 'react-tilt' যদি সেটি ব্যবহার করেন
+    include: ["react-parallax-tilt"], // বা 'react-tilt' যদি সেটি ব্যবহার করেন
   },
-})
-
-
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     outDir: 'dist'
-//   }
-// });
+});
